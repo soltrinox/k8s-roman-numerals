a8559c1a48df1ab4530e967396ff104a462e26d4
gkampitakis <gkabitakis@gmail.com>
2020-05-03 11:02:22 +0100

    [client] create router and pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { gql } from 'apollo-boost';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Main from './Components/Main';
+import Technologies from './Components/Technologies';
+
+// TODO: this should go to a different file
+//TODO: create a loader
+//TODO: technologies used [age ]
 
 const test = gql`
 	{
@@ -21,12 +28,19 @@ function App() {
 
 	return (
 		<div className="App">
-			<Header />
-			<main>
-				{data.arabics.map((value: any) => (
+			<Router>
+				<Header />
+				<main>
+					{/* {data.arabics.map((value: any) => (
 					<div key={value._id}>{value.arabic}</div>
-				))}
-			</main>
+				))} */}
+
+					<Switch>
+						<Route path="/technologies" exact component={Technologies} />
+						<Route path="/" component={Main} />
+					</Switch>
+				</main>
+			</Router>
 			<Footer />
 		</div>
 	);
--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header(props: any) {
 	return (
 		<header className="header">
-			<h1>Arabic-Roman Numerals</h1>
+			<h1 className="title">Arabic-Roman Numerals</h1>
+			<div className="navigation">
+				<Link to="/technologies" type="button">
+					About
+				</Link>
+			</div>
 		</header>
 	);
 }
